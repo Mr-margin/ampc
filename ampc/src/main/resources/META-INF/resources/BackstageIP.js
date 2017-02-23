0aac54eff0020b85feb497d8134f332bf4355870
327626497 <327626497@d4c7ba9f-2a4d-ce42-bd5f-1da9bc209f86>
2017-02-23 12:43:29 +0000

    列表

---
+++ ampc/src/main/resources/META-INF/resources/BackstageIP.js
@@ -0,0 +1,8 @@
+//var BackstageIP = "http://192.168.1.93:8091";
+var BackstageIP = "http://192.168.1.57:8082/ampc";
+//var BackstageIP = "http://localhost:8097";
+//var BackstageIP = "http://192.168.1.66:8081";
+//var BackstageIP = "http://makenv.ddns.net:8088/ecloud/";
+//var BackstageIP = "http://192.168.1.107:8091/";
+//var BackstageIP = "http://localhost:8091/ecloud/";
+//BackstageIP11 = "http://192.168.1.108:8090/dictionary/findByKeyName"
